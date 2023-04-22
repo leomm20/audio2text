142262c9b317ff14268c8a173a31ef4387d4f868
leomm20 <leonardo.maggiotti@gmail.com>
2023-04-22 13:33:29 -0300

    .

--- src/util.py
+++ src/util.py
@@ -1,5 +1,9 @@
 import subprocess
+from pathlib import Path
 
+file = 'audio.m4a'
+
+new_file = str(Path(file).stem) + '.wav'
 # run the FFmpeg command to convert the MP3/M4A file to WAV
-subprocess.run(["ffmpeg", "-i", "Grabación.m4a", "grabacion.wav"])
+subprocess.run(["ffmpeg", "-i", file, new_file])
 
