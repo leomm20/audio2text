3535a01dbbd141aa4c9316213b1e8e92c67c50f3
leomm20 <leonardo.maggiotti@gmail.com>
2023-04-22 14:08:46 -0300

    .

--- src/util.py
+++ src/util.py
@@ -1,9 +1,23 @@
+import os
 import subprocess
 from pathlib import Path
 
-file = 'audio.m4a'
 
-new_file = str(Path(file).stem) + '.wav'
-# run the FFmpeg command to convert the MP3/M4A file to WAV
-subprocess.run(["ffmpeg", "-i", file, new_file])
+def audio_to_wav(audio_file):
+    if not Path(audio_file.parent, 'processed'):
+        os.mkdir(Path(audio_file.parent, 'processed'))
+    try:
+        new_file = str(Path(audio_file).stem) + '.wav'
+        # run the FFmpeg command to convert the MP3/M4A file to WAV
+        subprocess.run(["ffmpeg", "-i", audio_file, Path(audio_file.parent, 'processed', audio_file.stem+'.wav')])
+    except:
+        print('Error')
 
+
+path = Path(os.getcwd(), 'audios_to_be_converted')
+if not path.exists():
+    print(f'Path {path} not found')
+    exit()
+
+for file in path.glob('*.*'):
+    audio_to_wav(file)
