ab2921280b6e11449e575971993c6328faedb7bf
leomm20 <leonardo.maggiotti@gmail.com>
2023-04-24 11:31:14 -0300

    1

--- src/util.py
+++ src/util.py
@@ -4,14 +4,14 @@ from pathlib import Path
 
 
 def audio_to_wav(audio_file):
-    if not Path(audio_file.parent, 'processed'):
+    if not Path(audio_file.parent, 'processed').exists():
         os.mkdir(Path(audio_file.parent, 'processed'))
     try:
         new_file = str(Path(audio_file).stem) + '.wav'
         # run the FFmpeg command to convert the MP3/M4A file to WAV
         subprocess.run(["ffmpeg", "-i", audio_file, Path(audio_file.parent, 'processed', audio_file.stem+'.wav')])
     except:
-        print('Error')
+        print('Error! Check environment variables! Add c:\\ffmpeg\\bin to path')
 
 
 path = Path(os.getcwd(), 'audios_to_be_converted')
